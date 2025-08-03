b50aee3f8cf3cdee7bd4f09c0b3e8ed22fee0c72
Manvi <159559667+manvip28@users.noreply.github.com>
2025-08-03 21:04:11 +0530

    Update client.py

--- client.py
+++ client.py
@@ -3,9 +3,10 @@ import requests
 payload = {
     "jsonrpc": "2.0",
     "method": "clean_duplicates",
-    "params": {"folder_path": r"E:\check"},  # change path
+    "params": {"folder_path": r"Path\To\Scan"},  # change path
     "id": 1
 }
 
 res = requests.post("http://localhost:5000/mcp", json=payload)
 print(res.json())
+
